6c8b41529526ab410827e1da41b793f022c8abe0
DanEscudero <daniel.escudero05@gmail.com>
2019-07-25 22:28:14 -0300

    Add initial image processing

--- labirinto/src/Main.hs
+++ labirinto/src/Main.hs
@@ -1,5 +1,15 @@
 module Main where
 
+import Data.Either
+import Codec.Picture
+
+testSave = saveBmpImage "src/testBmp.bmp"
+
+dynPixelSave img l = dynamicPixelMap (\x y -> l ++ [(x,y)])
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  img <- convertRGB8 $ readImage "src/MAZE01.png"
+  case img of
+    Left s -> print $ "ERRO: " ++ s
+    Right img -> testSave img
