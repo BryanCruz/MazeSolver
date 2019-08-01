1fe1f416f6fbf374279207003f6ae694e1ff201f
Filipi <filipi.brabo@gmail.com>
2019-08-01 00:14:16 -0300

    feat: add function to get first and second node of an edge

--- labirinto/src/Graph.hs
+++ labirinto/src/Graph.hs
@@ -5,3 +5,9 @@ data Node n = Node n deriving (Show, Eq)
 data Edge n = Edge (Node n) (Node n) deriving (Show, Eq)
 
 data Graph n = Graph [Node n] [Edge n] deriving (Show)
+
+getFst :: Edge a -> Node a
+getFst (Edge a b) = a
+
+getSnd :: Edge a -> Node a
+getSnd (Edge a b) = b
