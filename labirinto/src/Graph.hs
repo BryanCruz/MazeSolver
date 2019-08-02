b637a7d21f80582946d13489726a56d488324060
jonatas57 <jdjonatasduarte57@gmail.com>
2019-08-02 17:50:40 -0300

    refactor: change representation of graphs

--- labirinto/src/Graph.hs
+++ labirinto/src/Graph.hs
@@ -1,13 +1,21 @@
 module Graph where
 
-data Node n = Node n deriving (Show, Eq)
+newtype Node n = Node n deriving (Eq)
+newtype Edge n = Edge (Node n, Node n) deriving (Show, Eq)
+newtype Graph n = Graph [(Node n, [Edge n])]
 
-data Edge n = Edge (Node n) (Node n) deriving (Show, Eq)
+instance Show a => Show (Node a) where
+  show (Node x) = show x
 
-data Graph n = Graph [Node n] [Edge n] deriving (Show)
+instance Show a => Show (Graph a) where
+  show (Graph []) = ""
+  show (Graph (x:xs)) = show (fst x) ++ concatMap ((\x -> " -> " ++ show x) . getSnd) (snd x) ++ "\n" ++ show (Graph xs)
 
 getFst :: Edge a -> Node a
-getFst (Edge a b) = a
+getFst (Edge (a, b)) = a
 
 getSnd :: Edge a -> Node a
-getSnd (Edge a b) = b
+getSnd (Edge (a, b)) = b
+
+getAdjacent :: Eq a => Graph a -> Node a -> [Node a]
+getAdjacent (Graph vs) u = concat [map getSnd es | (n, es) <- vs, n == u]
--- labirinto/src/Main.hs
+++ labirinto/src/Main.hs
@@ -5,7 +5,13 @@ import Parser
 
 mazeName = "MAZE01"
 
+coords2Graph :: [(Int, Int)] -> Graph (Int, Int)
+coords2Graph ns = Graph [(Node (a, b), [Edge (Node (a, b), Node (c, d)) | (c, d) <- ns, manhattan (a, b) (c, d) == 1]) | (a, b) <- ns]
+  where
+    manhattan :: (Int, Int) -> (Int, Int) -> Int
+    manhattan (a, b) (c, d) = abs (a - c) + abs (b - d)
+
 main :: IO ()
 main = do
   mazeMatrix <- getMazeMatrix mazeName
-  print $ mazeMatrix
+  print mazeMatrix
