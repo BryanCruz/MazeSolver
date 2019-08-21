7cfbdc44858c5b02e47bea9f85572b64f2083cca
jonatas57 <jdjonatasduarte57@gmail.com>
2019-08-21 17:46:28 -0300

    refactor: add side walls in graphToMatrix

--- labirinto/src/GraphConverter.hs
+++ labirinto/src/GraphConverter.hs
@@ -3,13 +3,13 @@ module GraphConverter where
 import Graph
 
 graphToMatrix :: Graph (Int, Int) -> [[Int]]
-graphToMatrix g = [[if (x, y) `elem` nodePos then 0 else 1 | y <- [minY..maxY]] | x <- [minX..maxX]]
+graphToMatrix g = [[if (x, y) `elem` nodePos then 0 else -1 | y <- [minY..maxY]] | x <- [minX..maxX]]
   where
     nodes = getNodes g
     nodePos = [t | (Node t) <- nodes]
     (minX, maxX, minY, maxY) = getBounds nodePos (1000000000, 0, 1000000000, 0)
     getBounds :: [(Int, Int)] -> (Int, Int, Int, Int) -> (Int, Int, Int, Int)
-    getBounds [] bs = bs
+    getBounds [] (xMin, xMax, yMin, yMax) = (xMin, xMax, yMin - 1, yMax + 1)
     getBounds ((x, y):xs) (xMin, xMax, yMin, yMax) = getBounds xs (min x xMin, max x xMax, min y yMin, max y yMax)
 
 
@@ -18,10 +18,6 @@ matrixToGraph xss = graphInit $ concat [[(i, j) | (j, y) <- xs, y == 0] | (i, xs
   where
     indexed = [(fst x, zip [0..] $ snd x) | x <- indexLines]
     indexLines = zip [0..] xss
-
-{- [(0, [(0, 1) (1, 0) (2, 0)]),
-    (1, [0 1 0]),
-    (2, [0 0 1])] -}
 
 graphInit :: [(Int, Int)] -> Graph (Int, Int)
 graphInit xs = Graph [(Node x, [Edge (Node x, Node y) | y <- xs, manhattan x y == 1]) | x <- xs]
