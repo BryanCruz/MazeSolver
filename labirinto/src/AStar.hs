20b51e6484279643a4e82999819d27a9a95b7281
bryancruz <bryan.bialokur@gmail.com>
2019-08-25 23:16:52 -0300

    feat: add path in AStar
    Co-authored-by: jonatas57 <jdjonatasduarte57@gmail.com>
    Co-authored-by: Daniel Escudero <daniel.escudero05@gmail.com>
    Co-authored-by: Filipi Brabo <filipi.brabo@gmail.com>

--- labirinto/src/AStar.hs
+++ labirinto/src/AStar.hs
@@ -10,26 +10,40 @@ manhattan (Node (x1, y1)) (Node (x2, y2)) = abs (x1 - x2) + abs (y1 - y2)
 
 --    :: Maze             -> Initial node    -> End node        -> Path and Visited Nodes
 aStar :: Graph (Int, Int) -> Node (Int, Int) -> Node (Int, Int) -> ([Node (Int, Int)], [Node (Int, Int)])
-aStar g st end = ([], reverse $ aStar' [] [(0, st)] 0 g end)
+aStar g st end = (path, nodesVisited)
+  where
+    result       = aStar' [] [(0, (st, st))] 0 g end
+    path         = buildPath result end
+    nodesVisited = getVisited result
+
+buildPath :: Eq a => [(Node a, Node a)] -> Node a -> [Node a]
+buildPath [] _ = []
+buildPath (n:ns) pred
+  | uncurry (==) n = [fst n]
+  | fst n == pred  = buildPath ns (snd n)++[pred]
+  | otherwise      = buildPath ns pred
+
+getVisited ns = reverse [fst n | n <- ns]
 
 aStar' visited queue currentCost g end
   | st == end   = visited'
   | null queue' = []
   | otherwise   = recursion 
     where
-      st        = snd $ head queue
-      visited'  = st : visited
+      st        = fst $ snd $ head queue
+      pred      = snd $ snd $ head queue
+      visited'  = (st, pred) : visited
 
       recursion = aStar' visited' queue' currentCost' g end
 
-      neighbours          = filter (\n -> n `notElem` visited' && n `notElem` queueNodes) $ getAdjacent g st
+      neighbours          = map (\n -> (n, st)) $ filter (\n -> n `notElem` (map fst visited') && n `notElem` (map fst queueNodes)) $ getAdjacent g st
       neighboursEvaluated = zip (map f neighbours) neighbours
       queueNodes          = map snd $ tail queue
       queueReevaluated    = zip (map f queueNodes) queueNodes
       queue'              = sortOn fst $ zipWith (curry getMinor) queueReevaluated queue ++ neighboursEvaluated
         where
-          getMinor ((v, n), (v', _)) = (min v v', n)
+          getMinor ((v, (n, p)), (v', (_, p'))) = if (v <= v') then (v, (n, p)) else (v', (n, p'))
 
       heuristicsMultiplier = 1000
-      f n = currentCost' + 1000 * manhattan n end
+      f (n, _) = currentCost' + 1000 * manhattan n end
       currentCost' = currentCost + 1
